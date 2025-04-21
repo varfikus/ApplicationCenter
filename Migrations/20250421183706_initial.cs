a37a758806c4ad9f07c5927a9951059892e3c143
varfikus <varfikus@gmail.com>
2025-04-21 21:51:15 +0300

    db dump

---
+++ Migrations/20250421183706_initial.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationCenter.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    fullname = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    phone = table.Column<string>(type: "text", nullable: true),
+                    address = table.Column<string>(type: "text", nullable: true),
+                    placeofwork = table.Column<string>(type: "text", nullable: true),
+                    login = table.Column<string>(type: "text", nullable: true),
+                    passwordhash = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "applications",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    userid = table.Column<Guid>(type: "uuid", nullable: true),
+                    fullname = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    phone = table.Column<string>(type: "text", nullable: true),
+                    servicetype = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    status = table.Column<string>(type: "text", nullable: true, defaultValueSql: "'New'::text"),
+                    createdat = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    xmlpath = table.Column<string>(type: "text", nullable: true),
+                    files = table.Column<List<string>>(type: "text[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("applications_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "applications_userid_fkey",
+                        column: x => x.userid,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_applications_userid",
+                table: "applications",
+                column: "userid");
+
+            migrationBuilder.CreateIndex(
+                name: "users_login_key",
+                table: "users",
+                column: "login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "applications");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
